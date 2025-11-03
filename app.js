4a3f332bc277f35ff35c9885d28510b4f2d97013
mamun <mdmamunriyan361@gmail.com>
2025-11-03 15:16:21 +0600

    gh

--- app.js
+++
@@ -1,9 +0,0 @@
-let searchB = document.querySelector('.fa-solid fa-magnifying-glass');
-let searchDiv = document.querySelector('.search');
-
-searchB.addEventListener('click', () =>{
-
-    searchDiv.classList.toggle('fa-times');
-    searchB.classList.toggle('active');
-
-});
